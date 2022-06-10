aac9c4c9349341b95d9c6626c217020e3b40f126
AlejaeMC <alejandroanayadom@gmail.com>
2022-06-09 22:30:06 -0600

    Basic Pattern

---
+++ Patterns/src/Basic_Pattern/Square_Pattern.java
@@ -0,0 +1,17 @@
+package Basic_Pattern;
+
+public class Square_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int side = 9;
+		
+		for (int i = 0; i < side; i++) {
+			for (int j = 0; j < side; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
