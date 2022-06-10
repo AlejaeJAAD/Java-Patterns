24d54eeba30f2b8a5b21904fc8588ab711867d55
AlejaeMC <alejandroanayadom@gmail.com>
2022-06-09 23:11:25 -0600

    Triangle Pattern

---
+++ Patterns/src/Triangle_Pattern/Left_Side_Triangle_Pattern.java
@@ -0,0 +1,17 @@
+package Triangle_Pattern;
+
+public class Left_Side_Triangle_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size = 9;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
---
+++ Patterns/src/Triangle_Pattern/Right_Side_Triangle_Pattern.java
@@ -0,0 +1,19 @@
+package Triangle_Pattern;
+
+public class Right_Side_Triangle_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int size = 9;
+		
+		for (int i = 0; i < size; i++) {
+			for (int j = i; j < size-1; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}System.out.println("");
+		}
+	}
+
+}
